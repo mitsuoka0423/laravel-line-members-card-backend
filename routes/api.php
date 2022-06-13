beb9d29fa44ee40e0e405d0918592394db6ab5de
Takahiro Mitsuoka <tmitsuoka0423@gmail.com>
2022-06-13 21:43:30 +0900

    add airtable

--- routes/api.php
+++ routes/api.php
@@ -5,10 +5,9 @@ use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Route;
 use LINE\LINEBot;
 use LINE\LINEBot\Constant\HTTPHeader;
-use LINE\LINEBot\Event\FollowEvent;
 use LINE\LINEBot\Event\MessageEvent\TextMessage;
 use LINE\LINEBot\HTTPClient\CurlHTTPClient;
-use App\Models\Member;
+use Tapp\Airtable\Facades\AirtableFacade;
 
 /*
 |--------------------------------------------------------------------------
@@ -37,31 +36,38 @@ Route::post('/webhook', function (Request $request) use ($bot) {
     }
 
     $events = $bot->parseEventRequest($request->getContent(), $signature);
+    Log::debug(['$events' => $events]);
 
     collect($events)->each(function ($event) use ($bot) {
         if ($event instanceof TextMessage) {
-            return $bot->replyText($event->getReplyToken(), $event->getText());
-        }
-        if ($event instanceof FollowEvent) {
-            $user_id = $event->getUserId();
-            Log::debug($user_id);
+            if ($event->getText() === '会員カードを発行する')
+            {
+                $user_id = $event->getUserId();
+                Log::debug(['$user_id' => $user_id]);
 
-            $member = Member::find($user_id);
-            Log::debug($member);
+                $member = Airtable::where('UserId', $user_id)->get();
+                Log::debug(['$member' => $member->toArray()]);
 
-            if (empty($member)) {
-                $barcode_id = strval(rand(1000000000, 9999999999));
-                Log::debug($barcode_id);
+                if ($member->isEmpty()) {
+                    $barcode_id = strval(rand(1000000000, 9999999999));
+                    Log::debug($barcode_id);
 
-                $member = Member::firstOrCreate([
-                    'user_id' => $user_id,
-                    'barcode_id' => $barcode_id,
-                ]);
-                Log::debug('Member is created.');
-                Log::debug($member);
+                    $member = Airtable::firstOrCreate([
+                        'UserId' => $user_id,
+                        'Name' => $bot->getProfile($user_id)->getJSONDecodedBody()['displayName'],
+                        'MemberId' => $barcode_id,
+                    ]);
+                    Log::debug('Member is created.');
+                    Log::debug($member);
+
+                    return $bot->replyText($event->getReplyToken(), '会員カードを発行しました！');
+                } else {
+                    return $bot->replyText($event->getReplyToken(), '既に会員カードが発行されています。');
+                }
+
+            } else {
+                return $bot->replyText($event->getReplyToken(), $event->getText());
             }
-
-            return $bot->replyText($event->getReplyToken(), '[bot]友達登録されたよ！');
         }
     });
 
